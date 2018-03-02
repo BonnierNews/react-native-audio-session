79054441fd17747b752189d86dcd1985df1f4866
johankasperi <johan@kasperi.se>
2018-03-02 14:18:49 +0100

    Init

---
+++ ios/RNAudioSession.h
@@ -0,0 +1,13 @@
+//
+//  RNAudioSession.h
+//  RNAudioSession
+//
+//  Created by Daniel Lindström (DN) on 2018-01-17.
+//  Copyright © 2018 accosine. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RNAudioSession : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNAudioSession.m
@@ -0,0 +1,14 @@
+//
+//  RNAudioSession.m
+//  RNAudioSession
+//
+//  Created by Johan Kasperi (DN) on 2018-03-02.
+//
+
+#import "RNAudioSession.h"
+
+@implementation RNAudioSession
+
+RCT_EXPORT_MODULE();
+
+@end
